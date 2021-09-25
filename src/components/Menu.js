09e0ba30d2a8b2ac3dc8a3523d2f6ef10071e360
Allen Liao <namo50095@gmail.com>
2021-09-25 14:24:00 +0800

    finish forestageMenu and hamburger icon

--- src/components/Menu.js
+++ src/components/Menu.js
@@ -1,8 +1,10 @@
-import React from 'react'
+import React, { useState } from 'react'
 import styled from '@emotion/styled'
+import { useTheme } from '@emotion/react'
 import PropTypes from 'prop-types'
-import flexJustifyAlign from '../styles/flexJustifyAlign'
 import { Link } from 'react-router-dom'
+import flexJustifyAlign from '../styles/flexJustifyAlign'
+import { issueIcon, hamburgerIcon } from '../styles/icon'
 
 const MenuWrapper = styled.aside`
   position: fixed;
@@ -11,13 +13,50 @@ const MenuWrapper = styled.aside`
   left: 0;
   right: 20vw;
   background-color: ${({ theme }) => theme.secondary_900};
-  border-right: 1px solid ${({ theme }) => theme.secondary_300};
+  box-shadow: ${({ theme }) => theme.boxShadow};
   padding: 1.5rem;
+  transition: transform 500ms ease;
+  transform: ${({ isMenuOpen }) => !isMenuOpen && 'translate(-80vw)'};
 `
 
 const Nickname = styled.p`
   color: ${({ theme }) => theme.secondary_300};
   font-size: 0.9rem;
+`
+
+const ActivityWrapper = styled.div`
+  ${flexJustifyAlign('', 'flex-start')}
+  flex-direction: column;
+  gap: 1.5rem;
+  margin-top: 1.5rem;
+`
+
+const ActivityType = styled.div`
+  ${flexJustifyAlign('flex-start')}
+  gap: 1rem;
+`
+
+const ActivityDuration = styled.p`
+  font-size: 0.8rem;
+  color: ${({ theme }) => theme.secondary_300};
+`
+
+const ActivityDescription = styled.p`
+  font-size: 0.9rem;
+  line-height: 2rem;
+`
+
+const Button = styled.button`
+  background-color: ${({ theme }) => theme.primary};
+  color: ${({ theme }) => theme.secondary_900};
+  font-size: 1rem;
+  height: 2.5rem;
+  padding: 0 1.5rem;
+  border-radius: 1.125rem;
+  border: none;
+  margin: 0 auto;
+  display: block;
+  margin-top: 1.5rem;
 `
 
 const Footer = styled.footer`
@@ -40,13 +79,64 @@ const Footer = styled.footer`
   }
 `
 
-const Menu = ({ nickname }) => {
+const Hamburger = ({ onClick, className }) => {
+  const theme = useTheme()
   return (
-    <MenuWrapper>
+    <div onClick={onClick} className={className}>
+      {hamburgerIcon('2x', theme.primary)}
+    </div>
+  )
+}
+
+Hamburger.propTypes = {
+  onClick: PropTypes.func,
+  className: PropTypes.string
+}
+
+const StyledHamburger = styled(Hamburger)`
+  position: absolute;
+  top: 1rem;
+  right: 0;
+  transition: transform 500ms ease;
+  transform: translateX(${({ isMenuOpen }) => (isMenuOpen ? '-2rem' : '3rem')});
+`
+
+const Menu = ({
+  userId,
+  nickname,
+  title,
+  description,
+  beginDate,
+  finishDate
+}) => {
+  const theme = useTheme()
+  const [isMenuOpen, setIsMenuOpen] = useState(false)
+  return (
+    <MenuWrapper isMenuOpen={isMenuOpen}>
+      <StyledHamburger
+        isMenuOpen={isMenuOpen}
+        onClick={() => setIsMenuOpen((prev) => !prev)}
+      />
       <Nickname>{nickname}</Nickname>
+      <ActivityWrapper>
+        <ActivityType>
+          {issueIcon('2x', theme.secondary_300)}
+          <p>留言箱</p>
+        </ActivityType>
+        <p>{title}</p>
+        <ActivityDuration>
+          {beginDate} - {finishDate}
+        </ActivityDuration>
+        <ActivityDescription>{description}</ActivityDescription>
+      </ActivityWrapper>
+      {/* 判斷 userId 是否跟該 issue 的 userId 吻合，再顯示"進入此後台" */}
+      <Button>進入此後台</Button>
       <Footer>
-        <Link to="/">後台</Link>
-        <Link to="/">登出</Link>
+        {/* 後台頁面不會有後台按鈕 */}
+        {userId && <Link to="/">後台</Link>}
+        {userId && <Link to="/">登出</Link>}
+        {!userId && <Link to="/register">註冊</Link>}
+        {!userId && <Link to="/login">登入</Link>}
         <Link to="/">首頁</Link>
         <p>© Z-axis 2021</p>
       </Footer>
@@ -55,7 +145,12 @@ const Menu = ({ nickname }) => {
 }
 
 Menu.propTypes = {
-  nickname: PropTypes.string
+  userId: PropTypes.number,
+  nickname: PropTypes.string,
+  title: PropTypes.string,
+  description: PropTypes.string,
+  beginDate: PropTypes.string,
+  finishDate: PropTypes.string
 }
 
 export default Menu
