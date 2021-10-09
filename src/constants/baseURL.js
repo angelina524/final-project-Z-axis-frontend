b0183cc20569cbc46406d2d4c487ac6011526091
Benben <z2266109@gmail.com>
2021-10-10 01:42:43 +0800

    add router path

--- src/constants/baseURL.js
+++ src/constants/baseURL.js
@@ -1,6 +1,6 @@
 export const BACKEND_BASE_URL =
   process.env.NODE_ENV === 'production'
-    ? 'https://ben6515.tw'
+    ? 'https://ben6515.tw/api'
     : 'http://localhost:5001'
 
 export const FRONTEND_BASE_URL =
